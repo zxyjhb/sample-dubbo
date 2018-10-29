362a6480b4af367aa8903f6b997206fdd0e39846
jihaibo <568025297@qq.com>
2018-10-29 17:05:46 +0800

    cmt

--- dubbo-common/src/main/java/mac/sample/dubbo/common/bean/ReferenceBean.java
+++ dubbo-common/src/main/java/mac/sample/dubbo/common/bean/ReferenceBean.java
@@ -17,6 +17,25 @@ import mac.sample.dubbo.common.config.RegistryConfig;
 
 /**
  * 
+ * 
+ * 备注一下：
+ * bean的加载顺序
+ * 
+ * 
+ * 一、Spring装配Bean的过程
+ *1. 实例化;
+ *2. 设置属性值;
+ *3. 如果实现了BeanNameAware接口,调用setBeanName设置Bean的ID或者Name;
+ *4. 如果实现BeanFactoryAware接口,调用setBeanFactory 设置BeanFactory;
+ *5. 如果实现ApplicationContextAware,调用setApplicationContext设置ApplicationContext
+ *6. 调用BeanPostProcessor的预先初始化方法;
+ *7. 调用InitializingBean的afterPropertiesSet()方法;
+ *8. 调用定制init-method方法；
+ *9. 调用BeanPostProcessor的后初始化方法;
+ *
+ *Spring容器关闭过程
+ *1. 调用DisposableBean的destroy();
+ *2. 调用定制的destroy-method方法;
  * @author jihaibo
  *
  */
